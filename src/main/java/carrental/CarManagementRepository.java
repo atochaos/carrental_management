5ef24bd3fb11db01194de4080228462900e79bc9
atochaos <atochaos@gmail.com>
2020-08-05 20:42:20 +0900

    init

---
+++ src/main/java/carrental/CarManagementRepository.java
@@ -0,0 +1,10 @@
+package carrental;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.List;
+
+public interface CarManagementRepository extends PagingAndSortingRepository<CarManagement, Long>{
+
+    List<CarManagement> findByCarNo(String carNo);
+}
